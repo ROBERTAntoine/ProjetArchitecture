a06e9e7df431a2f5a09c9083037b326c6617aa43
Antoine <robert.antoine.89@gmail.com>
2017-09-12 10:26:16 +0200

    commit projet initial

---
+++ monAppliMavenGit/src/main/java/com/activ/mavenapps/monAppliMavenGit/App.java
@@ -0,0 +1,13 @@
+package com.activ.mavenapps.monAppliMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
